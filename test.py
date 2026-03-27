5bbb89b082cf3774f45517b9e1a79b73b781e0b9
moustafaelsafy2-prog <moustafa.elsafy2@gmail.com>
2026-03-27 15:15:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
+from google.cloud import vision
+import os
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\secure\gcloud-key.json"
+
+client = vision.ImageAnnotatorClient()
+
+with open("test.jpg", "rb") as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+response = client.text_detection(image=image)
+
+texts = response.text_annotations
+
+if texts:
+    print("النص المستخرج:\n")
+    print(texts[0].description)
+else:
+    print("لم يتم التعرف على نص")
